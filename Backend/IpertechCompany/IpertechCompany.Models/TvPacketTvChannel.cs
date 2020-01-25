4ed864777a02272e5aebffb67d3b4c613e898e91
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 18:16:25 +0100

    Add TvPacketTvChannel model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/TvPacketTvChannel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class TvPacketTvChannel : IValidation
+    {
+        public Guid TvPacketId { get; set; }
+        public Guid TvChannelId { get; set; }
+
+        public TvPacketTvChannel()
+        {
+
+        }
+
+        public TvPacketTvChannel(Guid tvPacketId, Guid tvChannelId)
+        {
+            TvPacketId = tvPacketId;
+            TvChannelId = tvChannelId;
+        }
+
+        public override string ToString()
+        {
+            return TvPacketId + ", " + TvChannelId;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!TvPacketId.Equals(null) && !TvChannelId.Equals(null)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
