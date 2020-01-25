3e4cce48cca4e0cf4819e65955f970281ff26ecc
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 18:06:23 +0100

    Add TvChannel model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/TvChannel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class TvChannel : IValidation
+    {
+        public Guid TvChannelId { get; set; }
+        public string Name { get; set; }
+        public string ImageLocation { get; set; }
+        public int PositionNumber { get; set; }
+        public bool TvBackwards { get; set; }
+
+        public TvChannel()
+        {
+
+        }
+
+        public TvChannel(Guid tvChannelId, string name, string imageLocation, int positionNumber, bool tvBackwards)
+        {
+            TvChannelId = tvChannelId;
+            Name = name;
+            ImageLocation = imageLocation;
+            PositionNumber = positionNumber;
+            TvBackwards = tvBackwards;
+        }
+
+        public override string ToString()
+        {
+            return TvChannelId + ", " + Name + ", " + ImageLocation + ", " + PositionNumber + ", " + TvBackwards;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!TvChannelId.Equals(null) && Name != null && PositionNumber != 0))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
