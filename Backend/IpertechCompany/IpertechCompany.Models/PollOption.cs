3d1971275dc85c9279663ea6f608d12bd6c935ae
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 19:31:32 +0100

    Add PollOption model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/PollOption.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class PollOption : IValidation
+    {
+        public Guid PollOptionId { get; set; }
+        public Guid PollId { get; set; }
+        public string AnswerText { get; set; }
+
+        public PollOption()
+        {
+
+        }
+
+        public PollOption(Guid pollOptionId, Guid pollId, string answerText)
+        {
+            PollOptionId = pollOptionId;
+            PollId = pollId;
+            AnswerText = answerText;
+        }
+
+        public override string ToString()
+        {
+            return PollOptionId + ", " + PollId + ", " + AnswerText;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!PollOptionId.Equals(null) && !PollId.Equals(null) && AnswerText != null))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
