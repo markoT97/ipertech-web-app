4e0816bd3c463d48657a64d9ed3d5ff423960a3c
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 19:44:47 +0100

    Add UserMessage model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/UserMessage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class UserMessage : IValidation
+    {
+        public Guid UserId { get; set; }
+        public Guid MessageId { get; set; }
+
+        public UserMessage()
+        {
+
+        }
+
+        public UserMessage(Guid userId, Guid messageId)
+        {
+            UserId = userId;
+            MessageId = messageId;
+        }
+
+        public override string ToString()
+        {
+            return UserId + ", " + MessageId;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!UserId.Equals(null) && !MessageId.Equals(null)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
