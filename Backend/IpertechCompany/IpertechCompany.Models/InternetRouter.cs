b6cb548891f69b4424e38a20bd368152f51891d2
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 17:52:11 +0100

    Add InternetRouter model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/InternetRouter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class InternetRouter : IValidation
+    {
+        public Guid InternetRouterId { get; set; }
+        public string Name { get; set; }
+        public string ImageLocation { get; set; }
+
+        public InternetRouter()
+        {
+
+        }
+
+        public InternetRouter(Guid internetRouterId, string name, string imageLocation)
+        {
+            InternetRouterId = internetRouterId;
+            Name = name;
+            ImageLocation = imageLocation;
+        }
+
+        public override string ToString()
+        {
+            return InternetRouterId + ", " + Name + ", " + ImageLocation;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!InternetRouterId.Equals(null) && Name != null))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
