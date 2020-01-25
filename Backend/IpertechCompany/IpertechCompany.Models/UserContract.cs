f6798f08f80c73a5ae101d141fb805edbc0a5d77
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 18:56:17 +0100

    Add UserContract model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/UserContract.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class UserContract : IValidation
+    {
+        public Guid UserContractId { get; set; }
+        public Guid PacketCombinationId { get; set; }
+        public int ContractDurationMonths { get; set; }
+
+        public UserContract()
+        {
+
+        }
+
+        public UserContract(Guid userContractId, Guid packetCombinationId, int contractDurationMonths)
+        {
+            UserContractId = userContractId;
+            PacketCombinationId = packetCombinationId;
+            ContractDurationMonths = contractDurationMonths;
+        }
+
+        public override string ToString()
+        {
+            return UserContractId + ", " + PacketCombinationId + ", " + ContractDurationMonths;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!UserContractId.Equals(null) && !PacketCombinationId.Equals(null) && ContractDurationMonths != 0))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
