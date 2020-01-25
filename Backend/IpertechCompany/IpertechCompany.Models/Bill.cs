4b295293dfc973e67256e7a2fa3bce64669b3c64
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 19:19:20 +0100

    Add Bill model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/Bill.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class Bill : IValidation
+    {
+        public Guid BillId { get; set; }
+        public Guid UserContractId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string CallNum { get; set; }
+        public string AccOfRecipient { get; set; }
+        public bool IsPaid { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+
+        public Bill()
+        {
+
+        }
+
+        public Bill(Guid billId, Guid userContractId, DateTime startDate, DateTime endDate, string callNum, string accOfRecipient, bool isPaid, decimal price, string currency)
+        {
+            BillId = billId;
+            UserContractId = userContractId;
+            StartDate = startDate;
+            EndDate = endDate;
+            CallNum = callNum;
+            AccOfRecipient = accOfRecipient;
+            IsPaid = isPaid;
+            Price = price;
+            Currency = currency;
+        }
+
+        public override string ToString()
+        {
+            return BillId + ", " + UserContractId + ", " + StartDate + ", " + EndDate + ", " + CallNum + ", " + AccOfRecipient + ", " + IsPaid + ", " + Price + ", " + Currency;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!BillId.Equals(null) && !UserContractId.Equals(null) && !StartDate.Equals(null) &&
+                  !EndDate.Equals(null) && CallNum != null && AccOfRecipient != null && Price != 0 && Currency != null))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
