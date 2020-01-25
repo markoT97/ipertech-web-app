5ab260b0a84d461cbb2c5fb73eb8e0f98ed5a842
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 18:45:06 +0100

    Add User model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class User : IValidation
+    {
+        public Guid UserId { get; set; }
+        public Guid UserContractId { get; set; }
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ImageLocation { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(Guid userId, Guid userContractId, string role, string firstName, string lastName, string gender, string email, string phoneNumber, string password
+         , string imageLocation)
+        {
+            UserId = userId;
+            UserContractId = userContractId;
+            Role = role;
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+            ImageLocation = imageLocation;
+
+        }
+
+        public override string ToString()
+        {
+            return UserId + ", " + UserContractId + ", " + Role + ", " + FirstName + ", " + LastName + ", " + Gender + ", " + Email + ", " + PhoneNumber + ", " + Password + ", " + ImageLocation;
+        }
+
+        public bool IsValid()
+        {
+
+            if (!(!UserId.Equals(null) && !UserContractId.Equals(null) && Role != null && FirstName != null &&
+                  LastName != null && Email != null && Password != null))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
