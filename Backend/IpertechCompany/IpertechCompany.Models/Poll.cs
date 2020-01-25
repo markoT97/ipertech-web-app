ce6b9dee8111ad2f06994895fd1a5e3485cea220
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 19:24:44 +0100

    Add Poll model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/Poll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class Poll : IValidation
+    {
+        public Guid PollId { get; set; }
+        public string Question { get; set; }
+        public int NumberOfVoters { get; set; }
+
+        public Poll()
+        {
+
+        }
+
+        public Poll(Guid pollId, string question, int numberOfVoters)
+        {
+            PollId = pollId;
+            Question = question;
+            NumberOfVoters = numberOfVoters;
+        }
+
+        public override string ToString()
+        {
+            return PollId + ", " + Question + ", " + NumberOfVoters;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!PollId.Equals(null) && Question != null))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
