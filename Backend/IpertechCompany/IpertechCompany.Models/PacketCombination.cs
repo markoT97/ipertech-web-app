7cd27e97810390e0ce19793640cb72fd13e4e4da
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 17:44:55 +0100

    Add PacketCombination model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/PacketCombination.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class PacketCombination : IValidation
+    {
+        public Guid PacketCombinationId { get; set; }
+        public string Name { get; set; }
+        public Guid InternetPacketId { get; set; }
+        public Guid TvPacketId { get; set; }
+        public Guid PhonePacketId { get; set; }
+
+        public PacketCombination()
+        {
+
+        }
+
+        public PacketCombination(Guid packetCombinationId, string name, Guid internetPacketId, Guid tvPacketId,
+            Guid phonePacketId)
+        {
+            PacketCombinationId = packetCombinationId;
+            Name = name;
+            InternetPacketId = internetPacketId;
+            TvPacketId = tvPacketId;
+            PhonePacketId = phonePacketId;
+        }
+
+        public override string ToString()
+        {
+            return PacketCombinationId + ", " + Name + ", " + InternetPacketId + ", " + TvPacketId + ", " + PhonePacketId;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!PacketCombinationId.Equals(null) && Name != null && !InternetPacketId.Equals(null)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
