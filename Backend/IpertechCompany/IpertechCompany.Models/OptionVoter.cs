d5e1c9cbd6f8b5fa472090801a3d82b27fd99c08
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 19:36:28 +0100

    Add OptionVoter model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/OptionVoter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class OptionVoter : IValidation
+    {
+        public Guid UserId { get; set; }
+        public Guid PollId { get; set; }
+        public Guid PollOptionId { get; set; }
+
+        public OptionVoter()
+        {
+
+        }
+
+        public OptionVoter(Guid userId, Guid pollId, Guid pollOptionId)
+        {
+            UserId = userId;
+            PollId = pollId;
+            PollOptionId = pollOptionId;
+        }
+
+        public override string ToString()
+        {
+            return UserId + ", " + PollId + ", " + PollOptionId;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!UserId.Equals(null) && !PollId.Equals(null) & !PollOptionId.Equals(null)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
