dc620a0e08196a0d2920e6f062c89dd7d87f277d
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 17:35:12 +0100

    Add Notification model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/Notification.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class Notification : IValidation
+    {
+        public Guid NotificationId { get; set; }
+        public Guid NotificationTypeId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageLocation { get; set; }
+
+        public Notification()
+        {
+
+        }
+
+        public Notification(Guid notificationId, Guid notificationTypeId, string title, string content, string imageLocation)
+        {
+            NotificationId = notificationId;
+            NotificationTypeId = notificationTypeId;
+            Title = title;
+            Content = content;
+            ImageLocation = imageLocation;
+        }
+
+        public override string ToString()
+        {
+            return NotificationId + ", " + NotificationTypeId + ", " + Title + ", " + Content + ", " + ImageLocation;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!NotificationId.Equals(null) && !NotificationTypeId.Equals(null) && Title != null &&
+                  ImageLocation != null))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
