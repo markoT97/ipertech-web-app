e280d5231ed15a7b975141b5fb4755e6b785b314
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 23:24:01 +0100

    Implement DbContext class library

---
+++ Backend/IpertechCompany/IpertechCompany.DbContext/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace IpertechCompany.DbContext
+{
+    class DbContext : IDbContext
+    {
+        public string ConnectionString { get; set; }
+        public SqlConnection Connection { get; set; }
+        public SqlCommand Command { get; set; }
+
+        public DbContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public IDbConnection Connect()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Backend/IpertechCompany/IpertechCompany.DbContext/IDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace IpertechCompany.DbContext
+{
+    interface IDbContext
+    {
+        IDbConnection Connect();
+    }
+}
