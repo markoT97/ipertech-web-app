6ae8c8d7339da4c8c96d5ce3592a67d9c0d724a6
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 17:27:10 +0100

    Add NotificationType model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/NotificationType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    public class NotificationType : IValidation
+    {
+        public Guid NotificationTypeId { get; set; }
+        public string Text { get; set; }
+        public int ImageWidth { get; set; }
+        public int ImageHeight { get; set; }
+
+        public NotificationType()
+        {
+
+        }
+
+        public NotificationType(Guid notificationTypeId, string text, int imageWidth, int imageHeight)
+        {
+            NotificationTypeId = notificationTypeId;
+            Text = text;
+            ImageWidth = imageWidth;
+            ImageHeight = imageHeight;
+        }
+
+        public override string ToString()
+        {
+            return NotificationTypeId + ", " + Text + ", " + ImageWidth + ", " + ImageHeight;
+        }
+        public bool IsValid()
+        {
+            if (!(!NotificationTypeId.Equals(null) && Text != null && ImageWidth != 0 && ImageHeight != 0))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Backend/IpertechCompany/IpertechCompany.Models/Validation/IValidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IpertechCompany.Models.Validation
+{
+    interface IValidation
+    {
+        bool IsValid();
+    }
+}
