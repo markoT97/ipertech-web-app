0e6334671f033ccb48344e28731942e716e6b681
Marko Tričković <marko.trickovic13@gmail.com>
2020-01-25 18:13:05 +0100

    Add TvPacket model

---
+++ Backend/IpertechCompany/IpertechCompany.Models/TvPacket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IpertechCompany.Models.Validation;
+
+namespace IpertechCompany.Models
+{
+    class TvPacket : IValidation
+    {
+        public Guid TvPacketId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public TvPacket()
+        {
+
+        }
+
+        public TvPacket(Guid tvPacketId, string name, decimal price)
+        {
+            TvPacketId = tvPacketId;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return TvPacketId + ", " + Name + ", " + Price;
+        }
+
+        public bool IsValid()
+        {
+            if (!(!TvPacketId.Equals(null) && Name != null && Price != 0))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
