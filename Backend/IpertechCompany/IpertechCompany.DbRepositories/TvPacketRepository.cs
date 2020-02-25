72f6a6874bb88879c298cc744b5ef0b208d95f0e
Marko Tričković <marko.trickovic13@gmail.com>
2020-02-25 14:24:30 +0100

    Add TvPacketRepository and Unit tests for it

---
+++ Backend/IpertechCompany/IpertechCompany.DbRepositories/TvPacketRepository.cs
@@ -0,0 +1,90 @@
+﻿using Dapper;
+using IpertechCompany.DbConnection;
+using IpertechCompany.IRepositories;
+using IpertechCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace IpertechCompany.DbRepositories
+{
+    public class TvPacketRepository : ITvPacketRepository
+    {
+        private readonly IDbContext _dbContext;
+
+        public TvPacketRepository(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool Delete(Guid tvPacketId)
+        {
+            var rowsAffected = 0;
+            using (var connection = _dbContext.Connect())
+            {
+                using (var command = (SqlCommand)connection.CreateCommand())
+                {
+                    const string query = "DELETE FROM packets.TvPacket" +
+                                         " WHERE TvPacketID = @TvPacketID";
+                    command.CommandText = query;
+                    command.Parameters.AddWithValue("@TvPacketID", SqlDbType.UniqueIdentifier).Value = tvPacketId;
+
+                    connection.Open();
+                    rowsAffected = command.ExecuteNonQuery();
+                }
+            }
+            return (rowsAffected > 0);
+        }
+
+        public IEnumerable<TvPacket> GetAll()
+        {
+            using (var connection = _dbContext.Connect())
+            {
+                const string query = "SELECT * FROM packets.TvPacket";
+                return connection.Query<TvPacket>(query);
+            }
+        }
+
+        public TvPacket Insert(TvPacket tvPacket)
+        {
+            var insertedTvPacket = tvPacket;
+            using (var connection = _dbContext.Connect())
+            {
+                using (var command = (SqlCommand)connection.CreateCommand())
+                {
+                    const string query = "INSERT INTO packets.TvPacket (TvPacketID, Name, Price)" +
+                                         " OUTPUT INSERTED.TvPacketID" +
+                                         " VALUES(@TvPacketID, @Name, @Price)";
+                    command.CommandText = query;
+                    command.Parameters.Add("@TvPacketID", SqlDbType.UniqueIdentifier).Value = tvPacket.TvPacketId;
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar, 50).Value = tvPacket.Name;
+                    command.Parameters.Add("@Price", SqlDbType.Decimal, 10).Value = tvPacket.Price;
+
+                    connection.Open();
+                    insertedTvPacket.TvPacketId = Guid.Parse(command.ExecuteScalar().ToString());
+                }
+            }
+            return insertedTvPacket;
+        }
+
+        public void Update(TvPacket tvPacket)
+        {
+            using (var connection = _dbContext.Connect())
+            {
+                using (var command = (SqlCommand)connection.CreateCommand())
+                {
+                    const string query = "UPDATE packets.TvPacket SET Name = @Name, Price = @Price" +
+                                         " WHERE TvPacketID = @TvPacketID";
+                    command.CommandText = query;
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar, 50).Value = tvPacket.Name;
+                    command.Parameters.Add("@Price", SqlDbType.Decimal, 10).Value = tvPacket.Price;
+                    command.Parameters.Add("@TvPacketID", SqlDbType.UniqueIdentifier).Value = tvPacket.TvPacketId;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Backend/IpertechCompany/IpertechCompany.Tests/Repositories/TvPacketRepositoryTests.cs
@@ -0,0 +1,80 @@
+﻿using IpertechCompany.DbConnection;
+using IpertechCompany.DbRepositories;
+using IpertechCompany.IRepositories;
+using IpertechCompany.Models;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace IpertechCompany.Tests.Repositories
+{
+    [TestFixture]
+    public class TvPacketRepositoryTests
+    {
+        private readonly IDbContext _dbContext;
+        private readonly ITvPacketRepository _tvPacketRepository;
+
+        public TvPacketRepositoryTests()
+        {
+            _dbContext = new DbContext("Data Source=DESKTOP-883AG4N\\SQLEXPRESS;Initial Catalog=IpertechCompany;Integrated Security=True");
+            _tvPacketRepository = new TvPacketRepository(_dbContext);
+        }
+
+
+        [Test]
+        public void GetAll_WithData_ReturnsListOfThreeTvPackets()
+        {
+            Assert.AreEqual(3, _tvPacketRepository.GetAll().Count());
+        }
+
+        [Test]
+        public void Insert_WithRequiredFields_ReturnsListWithFourTvPackets()
+        {
+            var tvPacket = new TvPacket(Guid.NewGuid(), "Insert Name", 1000);
+
+            _tvPacketRepository.Insert(tvPacket);
+            Assert.AreEqual(4, _tvPacketRepository.GetAll().Count());
+        }
+
+        [Test]
+        public void Insert_NullObject_ExpectsException()
+        {
+            Assert.Throws<NullReferenceException>(() => _tvPacketRepository.Insert(null));
+        }
+
+        [Test]
+        public void Update_WithRequiredFields_ReturnsTvPacket()
+        {
+            var tvPacket = new TvPacket
+            {
+                TvPacketId = Guid.Parse("D928DB62-A4C9-492E-BBDB-B15EF6E9E907"),
+                Name = "Update Name",
+                Price = 4000
+            };
+
+            _tvPacketRepository.Update(tvPacket);
+            var updatedTvPacket = _tvPacketRepository
+                .GetAll().Single(tp => tp.TvPacketId == tvPacket.TvPacketId);
+
+            Assert.AreEqual(tvPacket.Name, updatedTvPacket.Name);
+        }
+
+        [Test]
+        public void Update_NullObject_ExpectsException()
+        {
+            Assert.Throws<NullReferenceException>(() => _tvPacketRepository.Update(null));
+        }
+
+        [Test]
+        public void Delete_WhichExists_ReturnsTrue()
+        {
+            Assert.True(_tvPacketRepository.Delete(Guid.Parse("BB0D6A7A-7464-46E8-A446-FE9747FAA1B4")));
+        }
+
+        [Test]
+        public void Delete_WhichNotExists_ReturnsFalse()
+        {
+            Assert.False(_tvPacketRepository.Delete(Guid.NewGuid()));
+        }
+    }
+}
