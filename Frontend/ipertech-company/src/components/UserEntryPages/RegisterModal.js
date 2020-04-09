b1b3f6f7977f7099a05fd86a86b057d0a999b511
Marko Tričković <marko.trickovic13@gmail.com>
2020-04-09 14:44:08 +0200

    Implement register form validation

--- Frontend/ipertech-company/src/components/UserEntryPages/RegisterModal.js
+++ Frontend/ipertech-company/src/components/UserEntryPages/RegisterModal.js
@@ -6,55 +6,32 @@ import { connect } from "react-redux";
 import { setRegisterModalVisibility } from "../../redux/actions/modalsActions/actionCreators";
 import { insertUser } from "../../redux/actions/userActions/actionCreators";
 import { BACKEND_URL } from "../../redux/actions/backendServerSettings";
+import { Formik } from "formik";
+import { registerValidationSchema as schema } from "./../../shared/validation";
 
 export class RegisterModal extends Component {
   state = {
-    userContractId: "082c90f1-f513-4b2d-acf1-14b277d6d6c8",
-    firstName: "",
-    lastName: "",
-    gender: "",
-    email: "",
-    phoneNumber: "",
-    password: "",
-    passwordConfirm: "",
-
     showBillExample: false
   };
-  handleChangeInputValue = e => {
-    const { target } = e;
-    this.setState({ [target.name]: target.value });
-  };
-  handleOnSubmitRegisterForm = e => {
-    e.preventDefault();
+  handleOnSubmitRegisterForm = form => {
     this.props.insertUser({
       userContract: {
-        userContractId: this.state.userContractId
+        userContractId: form.userContractId
       },
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      gender: this.state.gender,
-      email: this.state.email,
-      phoneNumber: this.state.phoneNumber,
-      password: this.state.password,
-      passwordConfirm: this.state.passwordConfirm
+      firstName: form.firstName,
+      lastName: form.lastName,
+      gender: form.gender,
+      email: form.email,
+      phoneNumber: form.phoneNumber,
+      password: form.password,
+      passwordConfirm: form.passwordConfirm
     });
     this.props.setRegisterModalVisibility(false);
-
-    this.setState({
-      userContractId: "",
-      firstName: "",
-      lastName: "",
-      gender: "",
-      email: "",
-      phoneNumber: "",
-      password: "",
-      passwordConfirm: ""
-    });
   };
   render() {
+    const genders = ["Muški", "Ženski"];
     return (
       <Modal
-        onSubmit={this.handleOnSubmitRegisterForm}
         aria-labelledby="contained-modal-title-vcenter"
         centered
         show={this.props.modalsVisibility.registerModalVisibility}
@@ -66,178 +43,274 @@ export class RegisterModal extends Component {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group controlId="formRegisterContractID">
-              <Form.Label>Broj ugovora</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="btnGroupAddon" className="text-success">
-                    #
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  onChange={this.handleChangeInputValue}
-                  onFocus={() => this.setState({ showBillExample: true })}
-                  onBlur={() => this.setState({ showBillExample: false })}
-                  type="text"
-                  name="userContractId"
-                  placeholder="Unesite broj vašeg ugovora"
-                  value={this.state.userContractId}
-                />
-              </InputGroup>
-              <Form.Text className="text-muted">
-                Kliknite na polje za prikaz broja ugovora na računu
-              </Form.Text>
-            </Form.Group>
+          <Formik
+            validationSchema={schema}
+            onSubmit={this.handleOnSubmitRegisterForm}
+            initialValues={{
+              userContractId: "082c90f1-f513-4b2d-acf1-14b277d6d6c8",
+              firstName: "",
+              lastName: "",
+              gender: "",
+              email: "",
+              phoneNumber: "",
+              password: "",
+              passwordConfirm: ""
+            }}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              values,
+              touched,
+              isValid,
+              errors,
+              dirty
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formRegisterContractID">
+                  <Form.Label>Broj ugovora</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text
+                        id="btnGroupAddon"
+                        className="text-success"
+                      >
+                        #
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      onChange={handleChange}
+                      onFocus={() => this.setState({ showBillExample: true })}
+                      onBlur={() => this.setState({ showBillExample: false })}
+                      type="text"
+                      name="userContractId"
+                      placeholder="Unesite broj vašeg ugovora"
+                      value={values.userContractId}
+                      isValid={touched.userContractId && !errors.userContractId}
+                      isInvalid={!!errors.userContractId}
+                    />
+                    {errors.userContractId && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.userContractId}
+                      </Form.Control.Feedback>
+                    )}
+                  </InputGroup>
+                  <Form.Text className="text-muted">
+                    Kliknite na polje za prikaz broja ugovora na računu
+                  </Form.Text>
+                </Form.Group>
 
-            {this.state.showBillExample && (
-              <Image src={BACKEND_URL + "account/bill-example.svg"} fluid />
+                {this.state.showBillExample && (
+                  <Image src={BACKEND_URL + "account/bill-example.svg"} fluid />
+                )}
+
+                <Form.Group controlId="formRegisterFirstName">
+                  <Form.Label>Ime</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text
+                        id="btnGroupAddon"
+                        className="text-success"
+                      >
+                        I
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      onChange={handleChange}
+                      type="text"
+                      name="firstName"
+                      placeholder="Unesite vaše ime"
+                      value={values.firstName}
+                      isValid={touched.firstName && !errors.firstName}
+                      isInvalid={!!errors.firstName}
+                    />
+                    {errors.firstName && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.firstName}
+                      </Form.Control.Feedback>
+                    )}
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="formRegisterLastName">
+                  <Form.Label>Prezime</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text
+                        id="btnGroupAddon"
+                        className="text-success"
+                      >
+                        P
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      onChange={handleChange}
+                      type="text"
+                      name="lastName"
+                      placeholder="Unesite vaše prezime"
+                      value={values.lastName}
+                      isValid={touched.lastName && !errors.lastName}
+                      isInvalid={!!errors.lastName}
+                    />
+                    {errors.lastName && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.lastName}
+                      </Form.Control.Feedback>
+                    )}
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="formRegisterEmail">
+                  <Form.Label>Imejl</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text
+                        id="btnGroupAddon"
+                        className="text-success"
+                      >
+                        @
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      onChange={handleChange}
+                      type="email"
+                      name="email"
+                      placeholder="Unesite vašu imejl adresu"
+                      value={values.email}
+                      isValid={touched.email && !errors.email}
+                      isInvalid={!!errors.email}
+                    />
+                    {errors.email && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.email}
+                      </Form.Control.Feedback>
+                    )}
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="formRegisterPhoneNumber">
+                  <Form.Label>Broj telefona</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text
+                        id="btnGroupAddon"
+                        className="text-success"
+                      >
+                        <Icon.Phone />
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      onChange={handleChange}
+                      type="text"
+                      name="phoneNumber"
+                      placeholder="Unesite vaš broj telefona"
+                      value={values.phoneNumber}
+                      isValid={touched.phoneNumber && !errors.phoneNumber}
+                      isInvalid={!!errors.phoneNumber}
+                    />
+                    {errors.phoneNumber && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.phoneNumber}
+                      </Form.Control.Feedback>
+                    )}
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="formRegisterPassword">
+                  <Form.Label>Lozinka</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="btnGroupAddon">
+                        <Icon.Lock className="text-success" />
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      onChange={handleChange}
+                      type="password"
+                      name="password"
+                      placeholder="Lozinka"
+                      value={values.password}
+                      isValid={touched.password && !errors.password}
+                      isInvalid={!!errors.password}
+                    />
+                    {errors.password && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                      </Form.Control.Feedback>
+                    )}
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="formRegisterPasswordConfirm">
+                  <Form.Label>Potvrda lozinke</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="btnGroupAddon">
+                        <Icon.LockFill className="text-success" />
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      onChange={handleChange}
+                      type="password"
+                      name="passwordConfirm"
+                      placeholder="Potvrdite lozniku"
+                      value={values.passwordConfirm}
+                      isValid={
+                        touched.passwordConfirm && !errors.passwordConfirm
+                      }
+                      isInvalid={!!errors.passwordConfirm}
+                    />
+                    {errors.passwordConfirm && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.passwordConfirm}
+                      </Form.Control.Feedback>
+                    )}
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="formRegisterGender">
+                  <Form.Label>Pol</Form.Label>
+
+                  <Col>
+                    {genders.map((gender, i) => {
+                      return (
+                        <Form.Check
+                          key={i}
+                          onChange={handleChange}
+                          type="radio"
+                          label={gender}
+                          name="gender"
+                          id={"formRegisterGenderRadio" + i}
+                          value={gender}
+                          inline
+                          isValid={touched.gender && !errors.gender}
+                          isInvalid={!!errors.gender}
+                        />
+                      );
+                    })}
+                  </Col>
+                </Form.Group>
+
+                <hr />
+                <Button
+                  className="float-right ml-2"
+                  variant="light"
+                  onClick={() => this.props.setRegisterModalVisibility(false)}
+                >
+                  Odustani
+                </Button>
+                <Button
+                  className="float-right"
+                  type="submit"
+                  variant="success"
+                  disabled={!(isValid && dirty)}
+                >
+                  Potvrdi
+                </Button>
+              </Form>
             )}
-
-            <Form.Group controlId="formRegisterFirstName">
-              <Form.Label>Ime</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="btnGroupAddon" className="text-success">
-                    I
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  onChange={this.handleChangeInputValue}
-                  type="text"
-                  name="firstName"
-                  placeholder="Unesite vaše ime"
-                  value={this.state.firstName}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="formRegisterLastName">
-              <Form.Label>Prezime</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="btnGroupAddon" className="text-success">
-                    P
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  onChange={this.handleChangeInputValue}
-                  type="text"
-                  name="lastName"
-                  placeholder="Unesite vaše prezime"
-                  value={this.state.lastName}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="formRegisterEmail">
-              <Form.Label>Imejl</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="btnGroupAddon" className="text-success">
-                    @
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  onChange={this.handleChangeInputValue}
-                  type="email"
-                  name="email"
-                  placeholder="Unesite vašu imejl adresu"
-                  value={this.state.email}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="formRegisterPhoneNumber">
-              <Form.Label>Broj telefona</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="btnGroupAddon" className="text-success">
-                    <Icon.Phone />
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  onChange={this.handleChangeInputValue}
-                  type="text"
-                  name="phoneNumber"
-                  placeholder="Unesite vaš broj telefona"
-                  value={this.state.phoneNumber}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="formRegisterPassword">
-              <Form.Label>Lozinka</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="btnGroupAddon">
-                    <Icon.Lock className="text-success" />
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  onChange={this.handleChangeInputValue}
-                  type="password"
-                  name="password"
-                  placeholder="Lozinka"
-                  value={this.state.password}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="formRegisterPasswordConfirm">
-              <Form.Label>Potvrda lozinke</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="btnGroupAddon">
-                    <Icon.LockFill className="text-success" />
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  onChange={this.handleChangeInputValue}
-                  type="password"
-                  name="passwordConfirm"
-                  placeholder="Potvrdite lozniku"
-                  value={this.state.passwordConfirm}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="formRegisterGender">
-              <Form.Label>Pol</Form.Label>
-
-              <Col>
-                <Form.Check
-                  onChange={this.handleChangeInputValue}
-                  type="radio"
-                  label="Muški"
-                  name="gender"
-                  id="formRegisterGenderRadio1"
-                  value="Muški"
-                  inline
-                />
-                <Form.Check
-                  onChange={this.handleChangeInputValue}
-                  type="radio"
-                  label="Ženski"
-                  name="gender"
-                  id="formRegisterGenderRadio2"
-                  value="Ženski"
-                  inline
-                />
-              </Col>
-            </Form.Group>
-            <hr />
-            <Button
-              className="float-right ml-2"
-              variant="light"
-              onClick={() => this.props.setRegisterModalVisibility(false)}
-            >
-              Odustani
-            </Button>
-            <Button className="float-right" type="submit" variant="success">
-              Potvrdi
-            </Button>
-          </Form>
+          </Formik>
         </Modal.Body>
       </Modal>
     );
--- Frontend/ipertech-company/src/shared/validation.js
+++ Frontend/ipertech-company/src/shared/validation.js
@@ -24,3 +24,23 @@ export const loginValidationSchema = yup.object({
     .required(),
   password: yup.string().required()
 });
+
+export const registerValidationSchema = yup.object({
+  userContractId: yup.string().required(),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  gender: yup.string().required(),
+  email: yup
+    .string()
+    .email()
+    .required(),
+  phoneNumber: yup.string().required(),
+  password: yup
+    .string()
+    .min(8)
+    .required(),
+  passwordConfirm: yup
+    .string()
+    .min(8)
+    .required()
+});
